79d0e794dcb1efcf0bcc96af0a9b96ad76023ed2
breannams <breannamarie_s@hotmail.com>
2021-02-27 04:44:28 -0500

    commit tree 16721f9b4059c4aa84cd3bb797bc3638a3d055ae

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,12 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :show, through: :character
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    self.characters.map{|chara| "#{chara.name} - #{chara.show.name}"}
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
+require 'pry'
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actor, through: :characters
+    belongs_to :network
+
+    def actors_list
+     self.actor.map do |actors|
+
+        "#{actors.full_name}"
+
+     end
+    end
   
 end
